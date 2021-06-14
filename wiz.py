1672a8d964218a05b8898a4fb18e10ca4ad321e3
imiller142 <64618300+imiller142@users.noreply.github.com>
2021-06-14 08:35:38 -0500

    Create wiz.py

---
+++ wiz.py
@@ -0,0 +1,15 @@
+import random
+
+class Spell():
+  
+    def __init__(self, name, damage, effects):
+        self.name = name:
+        self.damage = damage:
+        self.effects = effects:
+    
+    def cast:
+        return self.damage
+            
+    
+      
+  
